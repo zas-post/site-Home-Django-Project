0b06f89010a0b38770c2a11c33070fd841cd7a9a
Alexander Zabolev <zas.post@gmail.com>
2023-12-20 23:10:08 +0300

    Changed files

--- main/views.py
+++ main/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.defaulttags import lorem
 
 
 def index(request):
-    context = {"title": "Home", "content": "Главная страница магазина - HOME"}
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+    }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О НАС",
+        "text_on_page": "«Поговорим не про Vim», олдам, сеньорам и тру-си-разработчикам на проверку: пользуетесь ли вы такими сочетаниями VS Code? Если же вы только начинаете программировать, влетаете в айти на реактивном самолете курсов по Питону, или просто не знаете ничего про хоткеи VS Code, возможно вам точно пригодится парочка сочетаний :)",
+    }
+    return render(request, "main/about.html", context)
