dd25f1e4ac6ecf30006fcdf9600df5c90be02263
Alexander Zabolev <zas.post@gmail.com>
2023-12-24 22:14:37 +0300

    Added second pagination option - GET params

--- goods/views.py
+++ goods/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
+
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
@@ -22,7 +25,7 @@ def catalog(request, category_slug, page=1):
 
 
 def product(request, product_slug):
-    product = Products().objects.get(slug=product_slug)
+    product = Products.objects.get(slug=product_slug)
 
     context = {"product": product}
 
