3aa65c31ba6518b94e12337c489c4351968307f8
Alexander Zabolev <zas.post@gmail.com>
2023-12-25 21:24:21 +0300

    Added The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from django import template
 
 from goods.models import Categories
@@ -8,3 +9,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
