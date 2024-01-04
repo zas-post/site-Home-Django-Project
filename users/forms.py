8c56d3c501582fa3540486f84f70e82046db5174
Alexander Zabolev <zas.post@gmail.com>
2024-01-05 00:08:46 +0300

    Profile form, controller and notifications are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 
 from users.models import User
 
@@ -35,8 +39,8 @@ class UserLoginForm(AuthenticationForm):
 #         )
 #     )
 
-class UserRegistrationForm(UserCreationForm):
 
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = (
@@ -54,3 +58,21 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
