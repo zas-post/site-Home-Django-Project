caa9883c2f9429d61dcb630d6b2d1553295595b3
Alexander Zabolev <zas.post@gmail.com>
2024-01-04 23:19:32 +0300

    Registration form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,21 +1,21 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
@@ -27,8 +27,19 @@ def login(request):
 
 
 def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        "title": "Home - Регистрация"
+        "title": "Home - Регистрация",
+        "form": form
     }
     return render(request, "users/registration.html", context)
 
@@ -41,4 +52,5 @@ def profile(request):
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('main:index'))
